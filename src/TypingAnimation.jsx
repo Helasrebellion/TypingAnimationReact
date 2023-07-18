00406f166ad0f446e4ec3ef40f905492afa422fc
Helasrebellion <49255017+Helasrebellion@users.noreply.github.com>
2023-07-18 12:16:36 -0400

    1 file modified

--- src/TypingAnimation.jsx
+++ src/TypingAnimation.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 const TypingAnimation = () => {
-  const textToType = '> Sylvia Mullins';
+  const textToType = '> Sylvia Mullins!';
   const typingSpeed = 100; // milliseconds per character
   const [typedText, setTypedText] = useState('');
   const [showCursor, setShowCursor] = useState(true);
+  const [startTime, setStartTime] = useState(Date.now());
   const timerRef = useRef(null);
 
   useEffect(() => {
@@ -31,8 +32,18 @@ const TypingAnimation = () => {
       setShowCursor((prevShowCursor) => !prevShowCursor);
     }, 500);
 
+    const resetAnimation = () => {
+      setTypedText('');
+      setShowCursor(true);
+      setStartTime(Date.now());
+    };
+
+    // Reset animation after 3 minutes (180,000 milliseconds)
+    const resetTimeout = setTimeout(resetAnimation, 180000);
+
     return () => {
       clearInterval(cursorTimer);
+      clearTimeout(resetTimeout);
     };
   }, [typedText]); 
 
@@ -41,7 +52,7 @@ const TypingAnimation = () => {
       <pre style={{ fontSize: 'inherit', display: 'inline', background: 'none', border: 'none' }}>
         {typedText}
       </pre>
-      {showCursor ? <span style={{ display: 'inline-block', verticalAlign: 'top', animation: 'blink 0.7s infinite' }}>&#124;</span> : ''}
+      {showCursor && <span style={{ display: 'inline-block', verticalAlign: 'top', animation: 'blink 0.7s infinite' }}>&#124;</span>}
     </h1>
   );
 };
